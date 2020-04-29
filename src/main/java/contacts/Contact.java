46d50e87d98032dc5677bd0fd16f37b4a631c495
ljurak <lucas.jurak@gmail.com>
2020-04-29 22:55:42 +0200

    Add logic for validation and updating contacts

--- src/main/java/contacts/Contact.java
+++ src/main/java/contacts/Contact.java
@@ -1,28 +1,63 @@
 package contacts;
 
+import java.util.regex.Pattern;
+
 public class Contact {
 
-    private final String firstName;
+    private static final Pattern phonePattern1 = Pattern.compile("^(\\+?[0-9a-z]+[ -])?(([0-9a-z]{2,3})|(\\([0-9a-z]{2,3}\\)))([ -][0-9a-z]{2,})*$", Pattern.CASE_INSENSITIVE);
 
-    private final String lastName;
+    private static final Pattern phonePattern2 = Pattern.compile("^\\+?(([0-9a-z]+)|(\\([0-9a-z]+\\)))$", Pattern.CASE_INSENSITIVE);
 
-    private final String phoneNumber;
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
 
     public Contact(String firstName, String lastName, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
+        if (validatePhoneNumber(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            System.out.println("Wrong number format!");
+        }
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (validatePhoneNumber(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            this.phoneNumber = null;
+            System.out.println("Wrong number format!");
+        }
+    }
+
+    public boolean hasNumber() {
+        return !(phoneNumber == null || phoneNumber.equals(""));
+    }
+
+    private boolean validatePhoneNumber(String phoneNumber) {
+        return phonePattern1.matcher(phoneNumber).matches() || phonePattern2.matcher(phoneNumber).matches();
     }
 }
