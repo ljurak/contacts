36d27c9f0177860bd9ac6e3097996f49f9058639
ljurak <lucas.jurak@gmail.com>
2020-05-01 23:15:12 +0200

    Make class abstract and remove validation from the class

--- src/main/java/contacts/Contact.java
+++ src/main/java/contacts/Contact.java
@@ -1,43 +1,19 @@
 package contacts;
 
-import java.util.regex.Pattern;
+import java.time.LocalDateTime;
 
-public class Contact {
+public abstract class Contact {
 
-    private static final Pattern phonePattern1 = Pattern.compile("^(\\+?[0-9a-z]+[ -])?(([0-9a-z]{2,3})|(\\([0-9a-z]{2,3}\\)))([ -][0-9a-z]{2,})*$", Pattern.CASE_INSENSITIVE);
+    protected String phoneNumber;
 
-    private static final Pattern phonePattern2 = Pattern.compile("^\\+?(([0-9a-z]+)|(\\([0-9a-z]+\\)))$", Pattern.CASE_INSENSITIVE);
+    protected final boolean isPerson;
 
-    private String firstName;
+    protected LocalDateTime creationDate;
 
-    private String lastName;
+    protected LocalDateTime updateDate;
 
-    private String phoneNumber;
-
-    public Contact(String firstName, String lastName, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        if (validatePhoneNumber(phoneNumber)) {
-            this.phoneNumber = phoneNumber;
-        } else {
-            System.out.println("Wrong number format!");
-        }
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    protected Contact(boolean isPerson) {
+        this.isPerson = isPerson;
     }
 
     public String getPhoneNumber() {
@@ -45,19 +21,22 @@ public class Contact {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        if (validatePhoneNumber(phoneNumber)) {
-            this.phoneNumber = phoneNumber;
-        } else {
-            this.phoneNumber = null;
-            System.out.println("Wrong number format!");
-        }
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
     }
 
     public boolean hasNumber() {
         return !(phoneNumber == null || phoneNumber.equals(""));
-    }
-
-    private boolean validatePhoneNumber(String phoneNumber) {
-        return phonePattern1.matcher(phoneNumber).matches() || phonePattern2.matcher(phoneNumber).matches();
     }
 }
