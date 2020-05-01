61f1b9fca150e97fa8bf51a743672d4c90e400c5
ljurak <lucas.jurak@gmail.com>
2020-05-01 23:20:42 +0200

    Add validator class for contacts

---
+++ src/main/java/contacts/ContactValidator.java
@@ -0,0 +1,30 @@
+package contacts;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Pattern;
+
+public class ContactValidator {
+
+    private static final Pattern phonePattern1 = Pattern.compile("^(\\+?[0-9a-z]+[ -])?(([0-9a-z]{2,3})|(\\([0-9a-z]{2,3}\\)))([ -][0-9a-z]{2,})*$", Pattern.CASE_INSENSITIVE);
+
+    private static final Pattern phonePattern2 = Pattern.compile("^\\+?(([0-9a-z]+)|(\\([0-9a-z]+\\)))$", Pattern.CASE_INSENSITIVE);
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        return phonePattern1.matcher(phoneNumber).matches() || phonePattern2.matcher(phoneNumber).matches();
+    }
+
+    public boolean validateBirthDate(String birthDate) {
+        try {
+            LocalDate.parse(birthDate, DateTimeFormatter.ofPattern("d-M-yyyy"));
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    public boolean validateGender(String gender) {
+        return "M".equalsIgnoreCase(gender) || "F".equalsIgnoreCase(gender);
+    }
+}
